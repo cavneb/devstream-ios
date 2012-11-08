12b97a36eb07362cf0ab4a02d0f5ee88ac038873
Eric Berry <eberry@skybox-pc.go1on1.local>
2012-11-08 14:48:11 -0700

    updated

--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -1,7 +1,7 @@
 class HomeScreen < ProMotion::Screen
-  def on_load
-    @posts = PostsScreen.new
-    @contributors = ContributorsScreen.new
-    @tab_bar = open_tab_bar @posts, @contributors
+  def will_appear
+    @posts        ||= PostsScreen.new
+    @contributors ||= ContributorsScreen.new
+    @tab_bar      ||= open_tab_bar @posts, @contributors
   end
 end  
